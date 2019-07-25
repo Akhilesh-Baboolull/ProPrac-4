6160c1fd6df9ea302c3c134130d5cbe8de7a6c89
Akhilesh07 <51824203+Akhilesh-Baboolull@users.noreply.github.com>
2019-07-25 14:01:23 +0400

    Added comments

--- Qu_speed.java
+++ Qu_speed.java
@@ -6,8 +6,8 @@
  * 
  */
 
-import java.util.Scanner; //
-import java.text.DecimalFormat;
+import java.util.Scanner; //import package for user input
+import java.text.DecimalFormat; //import package to round off
 
 
 public class Qu_speed {
@@ -18,42 +18,57 @@ public class Qu_speed {
 		Scanner myInput = new Scanner(System.in);
 		DecimalFormat df2 = new DecimalFormat("####.##");
 		
-		double distance;
-		double time;
-		double speed;
+		double distance; //variable for distance
+		double time; //variable for time
+		double speed; //variable for speed
 		int choice;
 		
 		do {
 		
-		System.out.println("Calculation Of Speed");
+		System.out.println("Calculation Of Speed"); //title
 		System.out.println("");
+		
+		do {
 		
 		System.out.print("Enter Distance travelled in metres: ");
 		distance = myInput.nextDouble();
 		
+		if (distance <= 0) { //Validation of distance
+			System.out.print("Invalid Distance entered! Please Re-Enter.");
+		}
+		
+		}while(distance <= 0);
+		
+		do {
 		System.out.print("Enter Time Taken in hours: ");
 		time = myInput.nextDouble();
 		
-		speed = distance / time;
+		if (time <= 0) { //validation of time
+			System.out.print("Invalid Time entered! Please Re-Enter.");
+		}
 		
-		System.out.println("The speed is " + df2.format(speed) + " m/h.");
+		}while(time <= 0);
+			
+		speed = distance / time; //calculation of speed
+		
+		System.out.println("The speed is " + df2.format(speed) + " m/h."); //printing the result of speed
 		
 		do {
-			
+			//menu if user wants to re-try the program
 		System.out.println("Choose 1 to continue");
 		System.out.println("Choose 2 to Exit");
 		
 		System.out.print("Enter choice: ");
-		choice = myInput.nextInt();
+		choice = myInput.nextInt(); //entering choice
 		
-		if(choice != 1 && choice != 2) {
+		if(choice != 1 && choice != 2) { //validating choice
 			
-			System.out.println("Invalid Choice entered! Please Re-Enter...");
+			System.out.println("Invalid Choice entered! Please Re-Enter..."); //validation of choice
 		}
 		
-		}while(choice != 1 && choice != 2);
+		}while(choice != 1 && choice != 2); //allowing user to re-enter choice
 		
-		}while(choice == 1);
+		}while(choice == 1); //Allowing user to re-try program
 		
 		
 		myInput.close();
